7620c1c4c8ba94c66233076dadf1783227daaf74
Chris Legault <chrislegault2011@gmail.com>
2012-09-20 12:01:34 -0300

    adding base interface for event manager

---
+++ source/Vienna/Eventing/IEventManager.cs
@@ -0,0 +1,11 @@
+﻿namespace Vienna.Eventing
+{
+    public interface IEventManager
+    {
+        bool AddListener<T>(long eventType, GameEventHandler<T> eventHandler) where T : class, IEventData;
+        bool RemoveListener<T>(long eventType, GameEventHandler<T> eventHandler) where T : class, IEventData;
+        bool TriggerEvent(IEventData eventData);
+        bool QueueEvent(IEventData eventData);
+        bool Update(int maxMilliseconds);
+    }
+}
