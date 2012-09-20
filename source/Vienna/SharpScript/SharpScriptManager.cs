1bffead254e2e1d30237c7b2b7aeec653dc96702
U-T4G\James.MacManus <macmanus.james@gmail.com>
2012-09-20 15:01:13 -0300

    trying to get app domains to work

--- source/Vienna/SharpScript/SharpScriptManager.cs
+++ source/Vienna/SharpScript/SharpScriptManager.cs
@@ -1,11 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿
+using System.Reflection;
 
 namespace Vienna.SharpScript
 {
-    class SharpScriptManager
+    public class SharpScriptManager
     {
+        /// <summary>
+        /// Thread safe static initializer
+        /// </summary>
+        private class Nested
+        {
+            internal static readonly SharpScriptManager SharpScriptManager = new SharpScriptManager();
+        }
+
+        /// <summary>
+        /// Gets the instance. 
+        /// </summary>
+        public static SharpScriptManager Instance
+        {
+            get { return Nested.SharpScriptManager; }
+        }
+
+        private const string GlobalContext = "__Global";
+        private readonly SharpScriptEnvironment _environment;
+
+        protected SharpScriptManager()
+        {
+            _environment = new SharpScriptEnvironment();
+            _environment.CreateContext(GlobalContext);
+        }
+
+        public void AddReference(Assembly assembly)
+        {
+            _environment.AddReference(GlobalContext, assembly);
+        }
+
+        public void Compile(string script)
+        {
+            _environment.Compile(GlobalContext, script);
+        }
+
+        public void Compile(string context, string script)
+        {
+            _environment.Compile(context, script);
+        }
+
+        public void CreateContext(string name)
+        {
+            _environment.CreateContext(name);
+        }
+
+        public void UnloadContext(string context)
+        {
+            _environment.UnloadContext(context);
+        }
+
+        public void GetInstance(string type, params object[] args)
+        {
+            _environment.GetInstance(GlobalContext, type, args);
+        }
+
     }
 }
