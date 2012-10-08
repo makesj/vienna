f4b16f04ceb695e3868fb672c76d8d81d76c28fa
James MacManus <macmanus.james@gmail.com>
2012-10-07 22:36:57 -0300

    a

---
+++ source/Vienna/Eventing/TestEvents.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Vienna.Eventing
+{
+    public static class TestEvents
+    {
+        public static Action<float, float> CameraMove;
+        public static Action<float> CameraRotate;
+        public static Action<float> CameraZoom;
+
+        public static Action ExitGame;
+    }
+}
